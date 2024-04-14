d64247b3c6b1ada757ab9c207d1496551e3808d8
Byron <58144346+LegenB@users.noreply.github.com>
2024-04-14 15:41:20 -0400

    Diseño responsivo para movil

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -191,43 +191,50 @@ export const Home = () => {
    
     return (
         
-        <div>     
+        <div>
+               
             <Info/>
-             <div className='sm:mx-2 md:mx-9'>
-                <div className=' grid xl:grid-cols-2 gap-y-8 xl:gap-5 xl:gap-y-0'>
+             <div className='mx-1 sm:mx-2 md:mx-9'>
+                <div className='grid xl:grid-cols-2 gap-y-8 xl:gap-5 xl:gap-y-0'>
                     <div className='w-full'> 
-                        <div className='flex items-center'>
-                            <img src={Edit} alt=""  className='size-10 mr-2'/>
-                            <h1 className='mb-1 text-2xl'>Crea tu JSON</h1>
-                        </div>
 
-                        <hr  className='border-violet-600'/>
-                        <form className='mt-3 max-h-[500px]  overflow-y-auto '>
+                        <div className='flex items-center'>
+                            <img src={Edit} alt="icon"  className='size-8 sm:size-9 md:size-10 mr-2'/>
+                            <h1 className='mb-1 sm:text-xl md:text-2xl'>Crea tu JSON</h1>
+                        </div>
+                        <hr className='border-violet-600'/>
+
+                        <form className='mt-3 max-h-[350px] min-h-[150px] md:max-h-[500px] overflow-y-auto '>
                             {Jarray.map((obj, index)=> (
-                                <ul key={index} className='flex items-center justify-center '>
+                                <ul key={index} className='flex items-center sm:justify-center text-xs sm:text-sm md:text-base     '>
+                                    <div className='hidden sm:block'>
+                                        <span className='text-xl sm:text-2xl text-violet-300 sm:mr-1'>{"{"}</span>
+                                    </div>
                                     
-                                    <span className='text-2xl text-violet-300 mr-1'>{"{"}</span>
                                     
-                                    <input type="text" placeholder='Clave ' name={`title.${index}`} value={formState[`title.${index}`] || ''} onChange={onInputChange} autocomplete="off"  className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-2/5' />
+                                    <input type="text" placeholder='Clave ' name={`title.${index}`} value={formState[`title.${index}`] || ''} onChange={onInputChange} autocomplete="off"  
+                                        className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-28 sm:w-2/5' />
                                     
                                     
-                                    <select className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95' 
+                                    <select className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-24' 
                                         onChange={(e) => onTypeChange(e, index)} 
                                         value={formState[`type.${index}`] || 'string'}>
                                         <option value="string">String</option>
                                         <option value="integer">Interger</option>
                                         <option value="bool">Bool</option>
                                     </select>
-                                    <span className='text-2xl text-violet-300 mx-1'>:</span>
-
+                                    
+                                    <div className='hidden sm:block'>
+                                        <span className='text-xl sm:text-2xl text-violet-300 sm:mx-1'>:</span>
+                                    </div>
                                     {formState[`type.${index}`] === 'bool' ? (
-                                        <div className='flex items-center w-2/5'>
+                                        <div className='flex items-center w-24 sm:w-2/5'>
                                            <input
                                             type="checkbox"
                                             name={`desc.${index}`}
                                             checked={formState[`desc.${index}`] || false}
                                             onChange={onInputChange}
-                                            className='m-1 p-1 size-5 '
+                                            className='m-1 p-1 size-4 sm:size-5 '
                                         /> 
                                             <span className={formState[`desc.${index}`] ? 'pl-2 text-blue-300' : 'pl-2 text-red-300'}>
                                                 {formState[`desc.${index}`] ? 'True' : 'False'}
@@ -243,16 +250,19 @@ export const Home = () => {
                                             value={formState[`desc.${index}`] || ''}
                                             onChange={onInputChange}
                                             autoComplete="off"
-                                            className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md border-2 border-sky-500/95 w-2/5'
+                                            className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md border-2 border-sky-500/95 w-24 sm:w-2/5'
                                         />
                                     )}
-                                    <span className='text-2xl text-violet-300  ml-1'>{"}"}</span>
-                                    <div className='flex justify-around items-center w-1/5'>
-                                        <button type="button" onClick={ () => onResetForm( index )} className='bg-red-200 rounded-full p-1 border border-red-300 hover:bg-red-400 transition group m-1 flex justify-center items-center'>
-                                            <img src={clean} alt="Limpiar"  className='size-6 group-hover:scale-105'/>
+                                    <div className='hidden sm:block'>
+                                        <span className='text-xl sm:text-2xl text-violet-300  sm:ml-1'>{"}"}</span>
+                                    </div>
+                                    
+                                    <div className='flex justify-center md:justify-around items-center sm:w-1/5'>
+                                        <button type="button" onClick={ () => onResetForm( index )} className='bg-red-200 rounded-full p-1 border border-red-400 hover:bg-red-400 transition group flex justify-center items-center'>
+                                            <img src={clean} alt="Limpiar"  className='size-4 sm:size-5 md:size-6 group-hover:scale-105'/>
                                         </button>
-                                        <button type="button" onClick={ () => onDeleteIndex( index )} className='bg-red-400 rounded-full p-1 border border-red-500 hover:bg-red-600 transition group flex justify-center items-center '>
-                                            <img src={papelera} alt="Limpiar"  className='size-6 group-hover:scale-105'/>
+                                        <button type="button" onClick={ () => onDeleteIndex( index )} className='ml-1 sm:ml-2 md:ml-0 bg-red-400 rounded-full p-1 border border-red-600 hover:bg-red-600 transition group flex justify-center items-center '>
+                                            <img src={papelera} alt="Limpiar"  className='size-4  sm:size-5 md:size-6 group-hover:scale-105'/>
                                         </button>
                                     </div>
                                     
@@ -261,8 +271,8 @@ export const Home = () => {
                             ))}
                         </form>
             
-                        <button className='bg-violet-600 p-1 m-1 border border-purple-800 rounded-lg transition hover:bg-violet-700 w-full flex items-center justify-center h-11' onClick={onAddObject}>
-                            <img src={add} alt="add" className='size-10 mr-3' /> Agregar
+                        <button className='bg-violet-600 p-1 m-1 border border-purple-800 rounded-lg transition hover:bg-violet-700 w-full flex items-center justify-center h-7 sm:h-8 md:h-11 text-sm md:text-base' onClick={onAddObject}>
+                            <img src={add} alt="add" className=' size-7 sm:size-8 md:size-10 mr-3' /> Agregar
                         </button>
 
                         
@@ -270,27 +280,27 @@ export const Home = () => {
                     
                     <div className='w-full'>
                         <div className='flex items-center'>
-                            <img src={Code} alt=""  className='size-10 mr-2'/>
-                            <h1 className='mb-1 text-2xl'>Code:</h1>
+                            <img src={Code} alt="icon"  className='size-8 sm:size-9 md:size-10 mr-2 '/>
+                            <h1 className='mb-1 sm:text-xl md:text-2xl'>Code:</h1>
                         </div>                      
                         <hr  className='border-violet-600'/>
                         {/* Aqui botones cuando es MENOR a xl*/}
                         <div className="block xl:hidden">
-                            <div className='flex justify-around mt-3'>
-                                <button className='bg-green-600 p-2 m-1 border  border-green-800 rounded-lg hover:scale-105 transition  hover:bg-green-700 w-[200px] flex items-center justify-center h-11' onClick={onCreateJSON}>
-                                    <img src={Martillo} alt="" className='size-9 mr-1'/> Crear JSON
+                            <div className='sm:flex justify-around mt-3'>
+                                <button className='bg-green-600 p-2  mb-3 sm:mb-0 sm:m-1 border  border-green-800 rounded-lg hover:scale-105 transition  hover:bg-green-700 w-full  sm:w-[200px] flex items-center justify-center h-7 sm:h-8 md:h-11 sm:text-sm md:text-base' onClick={onCreateJSON}>
+                                    <img src={Martillo} alt="" className='size-6 sm:size-7 md:size-9 mr-1'/> Crear JSON
                                 </button>
-                                <button className='bg-violet-600 p-2 m-1 border border-purple-800 rounded-lg hover:scale-105 transition hover:bg-violet-700 w-[200px] flex items-center justify-center h-11' onClick={copyToClipboard}>
-                                    <img src={Clip} alt="" className='size-9 mr-1'/>Copiar 
+                                <button className='bg-violet-600 p-2  mb-3 sm:mb-0 sm:m-1 border border-purple-800 rounded-lg hover:scale-105 transition hover:bg-violet-700 w-full sm:w-[200px] flex items-center justify-center h-7 sm:h-8 md:h-11 sm:text-sm md:text-base' onClick={copyToClipboard}>
+                                    <img src={Clip} alt="" className='size-6 sm:size-7 md:size-9 mr-1'/>Copiar 
                                 </button>
-                                <button className='bg-indigo-600 p-2 m-1 border border-indigo-800 rounded-lg hover:scale-105 transition hover:bg-indigo-700 w-[200px] flex items-center justify-center h-11' onClick={downloadTxtFile}>
-                                    <img src={Descarga} alt="" className='size-8 mr-1'/>
+                                <button className='bg-indigo-600 p-2  mb-3 sm:mb-0 sm:m-1 border border-indigo-800 rounded-lg hover:scale-105 transition hover:bg-indigo-700 w-full sm:w-[200px] flex items-center justify-center h-7 sm:h-8 md:h-11 sm:text-sm md:text-base' onClick={downloadTxtFile}>
+                                    <img src={Descarga} alt="" className='size-5 sm:size-6 md:size-8 mr-1'/>
                                     Descargar JSON 
                                 </button>
                             </div>
                         </div>
                         {/* Panel del Resultado del Codigo */}
-                        <div className='mb-14 xl:mb-0 bg-slate-800 rounded-md h-[500px] border-2 border-slate-950 p-2 mt-3 overflow-y-auto'>
+                        <div className='mb-14 xl:mb-0 bg-slate-800 rounded-md h-[350px] md:h-[500px] border-2 border-slate-950 p-2 mt-3 overflow-y-auto'>
                         
                             <pre className='' style={{ whiteSpace: 'pre-wrap' }}><code>{JSON.stringify(Jcontent, null, 2)}</code></pre>
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,26 +6,26 @@ import rayo from "../images/rayo.png"
 
 export const Info = () => {
   return (
-    <div className='sm:mx-2 md:mx-9 my-5 bg-slate-800 p-4 rounded-md'>
+    <div className='mx-1 sm:mx-2 md:mx-9 my-2 sm:my-3 md:my-5 bg-slate-800 p-4 rounded-md'>
         <div className='flex items-center mb-1'>
-            <img src={rayo} alt="" className='size-7 mr-1' />
-            <h2 className='text-xl'> Teclas Rapidas</h2>
+            <img src={rayo} alt="icon" className='size-5 sm:size-6 md:size-7 mr-1' />
+            <h2 className='text-md sm:text-lg md:text-xl'> Teclas Rapidas</h2>
 
         </div>
         
         
         <code>
-            <ul className='ml-4'>
+            <ul className='sm:ml-2 md:ml-4 text-xs sm:text-sm md:text-base'>
                 <li>
                     - <span className='text-blue-300'>Dentro </span> de un Campo de Texto apretar tecla <span className='text-yellow-200'>"Enter" </span> = Agregar Objeto 
                 </li>
-                <li>
+                <li className='hidden sm:block'>
                     - <span className='text-green-200'>Fuera </span> de un Campo de Texto apretar tecla <span className='text-yellow-200'>"Enter" </span> = Crear JSON
                 </li>
-                <li>
+                <li className='hidden sm:block'>
                     - <span className='text-green-200'>Fuera </span> de un Campo de Texto apretar tecla <span className='text-yellow-200'>"+" </span> = Agregar Nuevo Campo
                 </li>
-                <li>
+                <li className='hidden sm:block'>
                     - <span className='text-red-300'>Apretar</span> tecla <span className='text-yellow-200'>"Supr" </span> = Elimina el ultimo del JSON
                 </li>
             </ul>
