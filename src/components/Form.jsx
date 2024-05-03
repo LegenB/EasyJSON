2d4ff143e65c50e3d1cc71922d9fdfd1ac97d487
Byron <58144346+LegenB@users.noreply.github.com>
2024-05-03 17:27:36 -0400

    Ordenando codigo

---
+++ src/components/Form.jsx
@@ -0,0 +1,81 @@
+import Papelera from "../images/papelera.png"
+import Clean from "../images/clean.png"
+
+
+
+
+export const Form = ({ Jarray, formState, onInputChange, onTypeChange, onResetForm, onDeleteIndex }) => {
+    
+    
+    
+    return (
+           
+        <form className='mt-3 max-h-[350px] min-h-[150px] md:max-h-[500px] overflow-y-auto '>
+            {Jarray.map((obj, index)=> (
+                <ul key={index} className='flex items-center sm:justify-center text-xs sm:text-sm md:text-base     '>
+                    <div className='hidden sm:block'>
+                        <span className='text-xl sm:text-2xl text-violet-300 sm:mr-1'>{"{"}</span>
+                    </div>
+                    
+                    
+                    <input type="text" placeholder='Clave ' name={`title.${index}`} value={formState[`title.${index}`] || ''} onChange={onInputChange} autocomplete="off"  
+                        className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-28 sm:w-2/5' />
+                    
+                    
+                    <select className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-24' 
+                        onChange={(e) => onTypeChange(e, index)} 
+                        value={formState[`type.${index}`] || 'string'}>
+                        <option value="string">String</option>
+                        <option value="integer">Interger</option>
+                        <option value="bool">Bool</option>
+                        
+                    </select>
+                    
+                    <div className='hidden sm:block'>
+                        <span className='text-xl sm:text-2xl text-violet-300 sm:mx-1'>:</span>
+                    </div>
+                    {formState[`type.${index}`] === 'bool' ? (
+                        <div className='flex items-center w-24 sm:w-2/5'>
+                        <input
+                            type="checkbox"
+                            name={`desc.${index}`}
+                            checked={formState[`desc.${index}`] || false}
+                            onChange={onInputChange}
+                            className='m-1 p-1 size-4 sm:size-5 '
+                        /> 
+                            <span className={formState[`desc.${index}`] ? 'pl-2 text-blue-300' : 'pl-2 text-red-300'}>
+                                {formState[`desc.${index}`] ? 'True' : 'False'}
+                            </span>
+                        </div>
+
+                        
+                        ) : (
+                        <input
+                            type="text"
+                            placeholder='Valor'
+                            name={`desc.${index}`}
+                            value={formState[`desc.${index}`] || ''}
+                            onChange={onInputChange}
+                            autoComplete="off"
+                            className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md border-2 border-sky-500/95 w-24 sm:w-2/5'
+                        />
+                    )}
+                    <div className='hidden sm:block'>
+                        <span className='text-xl sm:text-2xl text-violet-300  sm:ml-1'>{"}"}</span>
+                    </div>
+                    
+                    <div className='flex justify-center md:justify-around items-center sm:w-1/5'>
+                        <button type="button" onClick={ () => onResetForm( index )} className='bg-red-200 rounded-full p-1 border border-red-400 hover:bg-red-400 transition group flex justify-center items-center'>
+                            <img src={Clean} alt="Limpiar"  className='size-4 sm:size-5 md:size-6 group-hover:scale-105'/>
+                        </button>
+                        <button type="button" onClick={ () => onDeleteIndex( index )} className='ml-1 sm:ml-2 md:ml-0 bg-red-400 rounded-full p-1 border border-red-600 hover:bg-red-600 transition group flex justify-center items-center '>
+                            <img src={Papelera} alt="Limpiar"  className='size-4  sm:size-5 md:size-6 group-hover:scale-105'/>
+                        </button>
+                    </div>
+                    
+                    
+                </ul>
+            ))}
+        </form>
+    )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ import Martillo from "../images/martillo.png"
 import Edit from "../images/Edit.png"
 import Descarga from "../images/Descarga.png"
 import { Info } from "./Info";
+import {Form} from './Form';
 
 
 export const Home = () => {
@@ -203,73 +204,7 @@ export const Home = () => {
                             <h1 className='mb-1 sm:text-xl md:text-2xl'>Crea tu JSON</h1>
                         </div>
                         <hr className='border-violet-600'/>
-
-                        <form className='mt-3 max-h-[350px] min-h-[150px] md:max-h-[500px] overflow-y-auto '>
-                            {Jarray.map((obj, index)=> (
-                                <ul key={index} className='flex items-center sm:justify-center text-xs sm:text-sm md:text-base     '>
-                                    <div className='hidden sm:block'>
-                                        <span className='text-xl sm:text-2xl text-violet-300 sm:mr-1'>{"{"}</span>
-                                    </div>
-                                    
-                                    
-                                    <input type="text" placeholder='Clave ' name={`title.${index}`} value={formState[`title.${index}`] || ''} onChange={onInputChange} autocomplete="off"  
-                                        className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-28 sm:w-2/5' />
-                                    
-                                    
-                                    <select className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md  border-2 border-sky-500/95 w-24' 
-                                        onChange={(e) => onTypeChange(e, index)} 
-                                        value={formState[`type.${index}`] || 'string'}>
-                                        <option value="string">String</option>
-                                        <option value="integer">Interger</option>
-                                        <option value="bool">Bool</option>
-                                    </select>
-                                    
-                                    <div className='hidden sm:block'>
-                                        <span className='text-xl sm:text-2xl text-violet-300 sm:mx-1'>:</span>
-                                    </div>
-                                    {formState[`type.${index}`] === 'bool' ? (
-                                        <div className='flex items-center w-24 sm:w-2/5'>
-                                           <input
-                                            type="checkbox"
-                                            name={`desc.${index}`}
-                                            checked={formState[`desc.${index}`] || false}
-                                            onChange={onInputChange}
-                                            className='m-1 p-1 size-4 sm:size-5 '
-                                        /> 
-                                            <span className={formState[`desc.${index}`] ? 'pl-2 text-blue-300' : 'pl-2 text-red-300'}>
-                                                {formState[`desc.${index}`] ? 'True' : 'False'}
-                                            </span>
-                                        </div>
-
-                                        
-                                        ) : (
-                                        <input
-                                            type="text"
-                                            placeholder='Valor'
-                                            name={`desc.${index}`}
-                                            value={formState[`desc.${index}`] || ''}
-                                            onChange={onInputChange}
-                                            autoComplete="off"
-                                            className='text-zinc-50 bg-slate-600 m-1 p-1 rounded-md border-2 border-sky-500/95 w-24 sm:w-2/5'
-                                        />
-                                    )}
-                                    <div className='hidden sm:block'>
-                                        <span className='text-xl sm:text-2xl text-violet-300  sm:ml-1'>{"}"}</span>
-                                    </div>
-                                    
-                                    <div className='flex justify-center md:justify-around items-center sm:w-1/5'>
-                                        <button type="button" onClick={ () => onResetForm( index )} className='bg-red-200 rounded-full p-1 border border-red-400 hover:bg-red-400 transition group flex justify-center items-center'>
-                                            <img src={clean} alt="Limpiar"  className='size-4 sm:size-5 md:size-6 group-hover:scale-105'/>
-                                        </button>
-                                        <button type="button" onClick={ () => onDeleteIndex( index )} className='ml-1 sm:ml-2 md:ml-0 bg-red-400 rounded-full p-1 border border-red-600 hover:bg-red-600 transition group flex justify-center items-center '>
-                                            <img src={papelera} alt="Limpiar"  className='size-4  sm:size-5 md:size-6 group-hover:scale-105'/>
-                                        </button>
-                                    </div>
-                                    
-                                    
-                                </ul>
-                            ))}
-                        </form>
+                        <Form Jarray={Jarray} formState={formState} onInputChange={onInputChange} onTypeChange={onTypeChange} onResetForm={onResetForm} onDeleteIndex={onDeleteIndex}></Form>               
             
                         <button className='bg-violet-600 p-1 m-1 border border-purple-800 rounded-lg transition hover:bg-violet-700 w-full flex items-center justify-center h-7 sm:h-8 md:h-11 text-sm md:text-base' onClick={onAddObject}>
                             <img src={add} alt="add" className=' size-7 sm:size-8 md:size-10 mr-3' /> Agregar
